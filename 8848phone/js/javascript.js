318f05c2d09d0a8ab505f42e278d4f4bd541ed62
E-Place <50448371+yphuang819126@users.noreply.github.com>
2019-05-14 17:17:32 +0800

    Add files via upload

---
+++ 8848phone/js/javascript.js
@@ -0,0 +1,12 @@
+$(function  () {
+	$.ajax({
+		type:"get",
+		url:"js/8848-0.json",
+		async:true,
+		success:function(data) {
+			$.each(data.navTag,function(v, t) {
+				$("<li><a href='#'>"+t+"</a></li>").appendTo($(".nav .clearfix"))
+			})
+		}	
+	});
+})
